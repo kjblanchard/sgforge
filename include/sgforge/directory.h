b1cc3111629f9ef953dd243271fdcda22c05fb17
Kevin Blanchard <kevin@supergoon.com>
2026-08-27 22:03:21 -0400

    do it

--- include/sgforge/directory.h
+++ include/sgforge/directory.h
@@ -29,7 +29,7 @@ void sgFreeDirectory(Directory* directory);
 Directory* sgDeserializeDirectoryFromBuffer(char* buf);
 Directory* sgDeserializeDirectoryFromBufferWithHeader(sgHeader* header, char* buf);
 Directory* sgDeserializeDirectoryFromFile(const char* filename);
-Directory* sgDeserializeDirectoryFromMemory( const char* filename, const void* data, size_t size)
+Directory* sgDeserializeDirectoryFromMemory( const char* filename, const void* data, size_t size);
 
 #ifdef __cplusplus
 }
